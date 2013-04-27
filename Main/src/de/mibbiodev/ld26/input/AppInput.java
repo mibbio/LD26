5a47c715643f1fb1fee840aef394b4747b7d7877
mibbio <contact@mibbiodev.de>
2013-04-27 20:53:38 +0200

    ESC closes game

---
+++ Main/src/de/mibbiodev/ld26/input/AppInput.java
@@ -0,0 +1,57 @@
+package de.mibbiodev.ld26.input;
+
+import com.badlogic.gdx.*;
+
+/**
+ * @author mibbio
+ */
+public class AppInput implements InputProcessor {
+    private static final int KEY_BACK = Input.Keys.ESCAPE;
+
+    Game game;
+
+    public AppInput(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if (keycode == KEY_BACK) Gdx.app.exit();
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;  // TODO Auto-generated method stub
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;  // TODO Auto-generated method stub
+    }
+}
